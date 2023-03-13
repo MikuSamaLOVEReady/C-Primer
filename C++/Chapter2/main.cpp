09847522753bc2a9896a39fcd82efc5d14fbc64d
ReayLoveMiku <543664432@qq.com>
2023-03-13 14:59:06 +0800

    second_update
    reduce chinesse

---
+++ C++/Chapter2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//""
+std::string globe_str;
+int global_int; //“0”
+
+int main() {
+    std::cout << globe_str << std::endl;
+    std::cout << global_int << std::endl;
+
+    int local_int;  //未定义的随机值
+    std::string local_str;
+
+    std::cout << local_str << std::endl;
+    std::cout << local_int << std::endl;
+
+    //todo:: 定义[分配空间] 声名
+    extern int i;  // 只是声明没有空间
+    i = 10;       // 初始化一个 extern 标记的变量【没有空间】
+
+    int j;          // 分空间，但没初始化
+
+
+
+    return 0;
+}
