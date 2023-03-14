f84249ba179420fc5e2b13820e480fdd6ced9ffe
ReayLoveMiku <543664432@qq.com>
2023-03-14 21:46:52 +0800

    3.14 - 21.46

--- C++/Chapter2/main.cpp
+++ C++/Chapter2/main.cpp
@@ -15,10 +15,21 @@ int main() {
     std::cout << local_int << std::endl;
 
     //todo:: 定义[分配空间] 声名
-    extern int i;  // 只是声明没有空间
-    i = 10;       // 初始化一个 extern 标记的变量【没有空间】
+    //extern int i;  // 只是声明没有空间=
+    //i = 10;       // 初始化一个 extern 标记的变量【没有空间】
 
-    int j;          // 分空间，但没初始化
+    int j;          // 分空间，但没初始化 或被分一个随机的数值
+
+    //NOTE:  Ref of pointer
+    int m = 43;
+    int n = 55;
+    int* p = &m;
+    int* &r = p;
+
+    r = &n;
+    std::cout<< *p <<std::endl;
+
+    //p = &m;
 
 
 
