7dbefafdc4d8aa8b6c3094b1a489810dfa6b11fd
ReayLoveMiku <543664432@qq.com>
2023-05-18 15:58:22 +0800

    5.18

---
+++ offer/offer-30.h
@@ -0,0 +1,36 @@
+//
+// Created by di on 2023/5/18.
+//
+
+#ifndef OFFER_OFFER_30_H
+#define OFFER_OFFER_30_H
+#include <stack>
+
+using namespace std;
+class MinStack {
+public:
+
+    stack<int> s1;
+
+    /** initialize your data structure here. */
+    MinStack() {
+
+    }
+
+    void push(int x) {
+    }
+
+    void pop() {
+
+    }
+
+    int top() {
+
+    }
+
+    int min() {
+
+    }
+};
+
+#endif //OFFER_OFFER_30_H
