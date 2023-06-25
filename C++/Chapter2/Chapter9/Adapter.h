22759c97c108c882b98ac432f798323750fc27d7
LuoRuidi <543664432@qq.com>
2023-06-25 09:36:45 +0800

    mac_update Chapter9 Finish

---
+++ C++/Chapter2/Chapter9/Adapter.h
@@ -0,0 +1,29 @@
+//
+// Created by ÂÞÈðµÑ on 2023/6/24.
+//
+
+#ifndef CHAPTER7_ADAPTER_H
+#define CHAPTER7_ADAPTER_H
+#include <stack>
+#include <queue>
+
+void adaptor_deque(){
+    deque<int> deq;
+    stack<int> stk(deq);
+}
+
+void priority_queue_vs_queue(){
+    priority_queue<int> p_queue;
+    p_queue.push(0);
+    p_queue.push(1);
+    p_queue.push(2);
+    p_queue.push(3);
+    auto re = p_queue.top();
+
+}
+
+
+
+
+
+#endif //CHAPTER7_ADAPTER_H
